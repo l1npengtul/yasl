e2ddfd3577b25509efd7c44ec2cbf4ec79b0c170
l1npengtul <l1npengtul@protonmail.com>
2026-08-20 17:37:14 +0900

    sqlx

--- package.nix
+++ package.nix
@@ -14,8 +14,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "poise-0.6.1" = "sha256-LYGoUUXFy3gEpnQtWxNsBbcKdeBallENTq1mfvVkTn4=";
-      "serenity-0.12.5" = "sha256-SEuJbFx3f7/aMWZsU5lwUDuz5SoFGm9DTVhRVdA0AmU=";
+      "poise-0.6.1" = lib.fakeHash;
     };
   };
 
@@ -24,12 +23,4 @@ rustPlatform.buildRustPackage (finalAttrs: {
     sqlx-cli
   ];
   buildInputs = [ openssl ];
-
-  configurePhase = ''
-    export DATABASE_URL="sqlite://yasl.sql"
-    sqlx database create
-    sqlx database setup
-    cargo sqlx prepare
-  '';
-
 })
